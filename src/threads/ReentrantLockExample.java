51421da6f25e19258c0bccff31d64dbd13281693
Pedro Ulhoa <pedroulhoa@live.com>
2023-05-23 20:40:21 -0300

    Reentrant lock examples

---
+++ src/threads/ReentrantLockExample.java
@@ -0,0 +1,33 @@
+package threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantLockExample {
+
+    private static int i = -1;
+
+    private static Lock lock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Runnable r1 = () -> {
+            lock.lock();
+            String name = Thread.currentThread().getName();
+            i++;
+            System.out.println(name + ": " + i);
+            lock.unlock();
+        };
+
+        for (int i = 0; i < 6; i++) {
+            executor.execute(r1);
+        }
+
+        executor.shutdown();
+    }
+
+}
+
